6dd1f2cc47878d55deaa59aef07ff83b3c997962
Ronan Downes <ronandownes@users.noreply.github.com>
2026-08-15 10:23:35 +0100

    Add shared MathJax configuration

---
+++ assets/mathjax-config.js
@@ -0,0 +1,22 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]']]
+  },
+  svg: {
+    fontCache: 'global'
+  },
+  options: {
+    enableMenu: true,
+    menuOptions: {
+      settings: {
+        zoom: 'Click',
+        zscale: '200%',
+        renderer: 'SVG',
+        inTabOrder: true,
+        assistiveMml: true,
+        overflow: 'Scroll'
+      }
+    }
+  }
+};
